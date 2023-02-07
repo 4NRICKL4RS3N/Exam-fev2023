55bcbb9994fc0e0bf70f311f2ff4277e69785613
rakotoarinosy-teddy <rakotoarinosyteddy8@gmail.com>
2023-02-07 13:23:41 +0000

    New Login

--- application/views/login.php
+++ application/views/login.php
@@ -10,17 +10,24 @@
     <title>Document</title>
 </head>
 <body>
-<form>
-    <h2>Page Login</h2>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    <button class="btn btn-default" type="submit">Valider</button>
-  </div>
-</form>
+<div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+    
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </body>
 </html>
