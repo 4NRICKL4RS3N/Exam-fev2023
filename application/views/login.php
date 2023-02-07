55a2b951491c854e2b4bc6ea04ea63c1ee928390
rakotoarinosy-teddy <rakotoarinosyteddy8@gmail.com>
2023-02-07 13:04:16 +0000

    Bootstrap Login

---
+++ application/views/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+<form>
+    <h2>Page Login</h2>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <button class="btn btn-default" type="submit">Valider</button>
+  </div>
+</form>
+</body>
+</html>
