2eb8e49fa0e4249ad72d5d10aff5a7585afede0d
L4RS3N <rakotorazafya@gmail.com>
2023-02-07 14:33:46 +0300

    Update model_login.php

--- application/models/model_login.php
+++ application/models/model_login.php
@@ -15,6 +15,7 @@ class Login extends CI_Model {
         $sql = sprintf($sql, $this->db->escape($idUser));
         $query = $this->db->query($sql);
         $result = $query->row_array();
+        $test;
         return $result;
     }
 
