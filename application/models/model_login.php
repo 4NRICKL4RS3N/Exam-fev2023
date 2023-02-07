69786762a0c8c35918ad870ba945710d5aa95d75
L4RS3N <rakotorazafya@gmail.com>
2023-02-07 14:21:08 +0300

    model login
    maka user rehetra sy sy user ray

---
+++ application/models/model_login.php
@@ -0,0 +1,22 @@
+<?php
+if(! defined('BASEPATH')) exit('No direct script acces allowed');
+
+class Login extends CI_Model {
+
+    public function getUsers() {
+        $sql = "select * from users";
+        $query = $this->db->query($sql);
+        $result = $query->result_array();
+        return $result;
+    }
+
+    public function getUsersById($idUser) {
+        $sql = "select * from users where idUser = %s";
+        $sql = sprintf($sql, $this->db->escape($idUser));
+        $query = $this->db->query($sql);
+        $result = $query->row_array();
+        return $result;
+    }
+
+}
+?>
