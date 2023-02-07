68e1b14153eac67945b5bdee6d469ca3de70eb51
L4RS3N <rakotorazafya@gmail.com>
2023-02-07 20:46:33 +0300

    Create model_user.php

---
+++ application/models/model_user.php
@@ -0,0 +1,20 @@
+<?php
+if(! defined('BASEPATH')) exit('No direct script acces allowed');
+
+class Model_Objet extends CI_Model {
+
+    public function getObjetByUser($idUser) {
+        $sql = "select * from objet where idUser = %s";
+        $sql = sprintf($sql, $this->db->escape($idUser));
+        $query = $this->db->query($sql);
+        $result = $query->result_array();
+        return $result;
+    }
+
+    public function acceptChange($idEchange) {
+        
+    }
+
+} 
+
+?>
