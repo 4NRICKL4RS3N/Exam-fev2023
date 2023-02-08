2ec4ed1f55db2ff6158ea8b2c684e71a743fa1bf
L4RS3N <rakotorazafya@gmail.com>
2023-02-08 04:29:06 +0300

    accueil et fonction modif

--- application/models/model_Objet.php
+++ application/models/model_Objet.php
@@ -7,18 +7,25 @@
         {
             $sql="select * from objet";
             $query = $this->db->query($sql);
-            $result = $query->result_array();
+            $result = $this->db->result_array();
             return $result;
         }
 
         public function getObjetsById($mail)
         {
             $this->load->model('model_user');
-            $id =  getIdByMail($mail);
+            $id = $this->model_user->getIdByMail($mail);
             $req="select * from objet where idUser = %s";
             $req = sprintf($req , $this->db->escape($id));
             $query = $this->db->query($req);
-            $result = $query->result_array();
+            $result = $this->db->result_array();
+            return $result;
+        }
+
+        public function getCategories() {
+            $sql="select * from category";
+            $query = $this->db->query($sql);
+            $result = $this->db->result_array();
             return $result;
         }
     }
